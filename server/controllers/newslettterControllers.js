0b0ea646b552587008095f79b5e43366802eb844
Harmeet <harmeet373.be22@chitkara.edu.in>
2024-11-18 09:35:20 +0530

    Added newsletter

--- server/controllers/newslettterControllers.js
+++ server/controllers/newslettterControllers.js
@@ -1,9 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const newsletter = require("../model/newsletterModel");
+const User = require("../model/newsletterModel");
 
 const getNewsletter = asyncHandler(async (req, res) => {
     try {
         const data = await newsletter.find({});
+        if(!data){
+            return res.status(404).json({message: "NewsLetter doesn't exist"})
+        }
+        return res.status(200).json();
         
     } catch (err) {
         return res.status(404).json({err:err.message});
@@ -11,11 +16,25 @@ const getNewsletter = asyncHandler(async (req, res) => {
 })
 
 const createNewsletter = asyncHandler(async (req, res) => {
-    // const { title, authot, date, imageUrl, description } = req.body;
-    // if (!title || !auhtor || !date || !imageUrl || !description) {
-    //     res.status(400);
-    //     throw new Error("Please fill all fields");
-    // }
+    const { title, author, date, imageUrl, description } = req.body;
+    if (!title || !author || !date || !imageUrl || !description) {
+        res.status(400);
+        throw new Error("Please fill all fields");
+    }
+
+    const userExists = await User.findOne({ title });
+    if (userExists) {
+        return res.status(400).json({ message: "NewsLetter exists" });
+    }
+
+    const newUser = await User.create({
+        title,
+        author,
+        date,
+        imageUrl,
+        description,
+    });
+    res.status(201).json({ message: "User Registered Successfully", user: newUser });
 })
 
 module.exports = { getNewsletter, createNewsletter };
